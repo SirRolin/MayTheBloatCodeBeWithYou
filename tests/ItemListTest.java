ddcbafda2afcf06fe99efaa7cd29f7da46cc447f
chris4567890 <starchristian4@hotmail.com>
2023-08-23 13:05:31 +0200

    horrible unittests
    did a bunch of horrible unit tests and made a getItems function shamelessly stolen from stackoverflow

---
+++ tests/ItemListTest.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemListTest {
+
+    @Test
+    void addItem() {
+        Item apple = new Item("apple");
+        Item banana = new Item("banana");
+        Item cerial = new Item("cerial");
+        ItemList itemList = new ItemList("breakfeast");
+        itemList.addItem(apple);
+        itemList.addItem(banana);
+        itemList.addItem(cerial);
+        assertNotNull(itemList.getItem(apple.getName()));
+
+    }
+
+    @Test
+    void toggleItem() {
+
+        Item apple = new Item("apple");
+        ItemList itemList2 = new ItemList("snack");
+        itemList2.addItem(apple);
+        boolean checkCheck;
+        if (itemList2.items.contains(apple)){
+            checkCheck = apple.isCheck();
+            System.out.println(checkCheck);
+            apple.toggleCheck();
+            checkCheck = apple.isCheck();
+            System.out.println(checkCheck);
+            System.out.println("it worked!");
+        }else {
+            System.out.println("object not found please try again");
+        }
+
+    }
+
+    @Test
+    void removeItem() {
+        Item apple = new Item("apple");
+        Item banana = new Item("banana");
+        ItemList itemList3 = new ItemList("snack");
+        itemList3.addItem(apple);
+        itemList3.addItem(banana);
+
+        assertNotNull(itemList3.getItem(apple.getName()));
+        itemList3.removeItem("apple");
+        assertNull(itemList3.getItem(apple.getName()));
+    }
+
+    @Test
+    void changeAmount() {
+        ItemList itemList = new ItemList("quicksnacks");
+        Item bitcoinminerInitiated = new Item("tottally not a bitcoin miner");
+        Item banana = new Item("banana");
+        itemList.addItem(bitcoinminerInitiated);
+        itemList.addItem(banana);
+        assertEquals(0,banana.getAmount());
+        if(itemList.getItem(banana.getName()) != null){
+            banana.setAmount(10);
+        }
+        assertEquals(10,banana.getAmount());
+
+    }
+}
